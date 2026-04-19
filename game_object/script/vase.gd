2b2caf2087b75e31953d6af9934632fa621edc70
Sitthata <aprils96950@gmail.com>
2026-04-19 21:32:06 +0700

    feat: make vase be able to push another

--- game_object/script/vase.gd
+++ game_object/script/vase.gd
@@ -7,8 +7,9 @@ signal lifted_from_plate
 func push(direction: Vector2, push_speed: float) -> void:
 	velocity = direction * push_speed
 	move_and_slide()
-	velocity = Vector2.ZERO  # stop after one frame push
-
-
-func _on_pressure_plate_pressed() -> void:
-	pass # Replace with function body.
+	for i in get_slide_collision_count():
+		var col = get_slide_collision(i)
+		var collider = col.get_collider()
+		if collider and collider.has_method("push"):
+			collider.push(col.get_normal() * -1, push_speed)
+	velocity = Vector2.ZERO
