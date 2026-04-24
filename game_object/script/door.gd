445f62c30112464f864ea51a2af7744e4afb31f6
Sitthata <aprils96950@gmail.com>
2026-04-24 15:48:52 +0700

    feat: add occluder to door to make it responsive while closing

--- game_object/script/door.gd
+++ game_object/script/door.gd
@@ -2,18 +2,22 @@ extends StaticBody2D
 
 @onready var _collision: CollisionShape2D = $CollisionShape2D
 @onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var _occluder: LightOccluder2D = $LightOccluder2D
 
 func _ready() -> void:
 	_collision.set_deferred("disabled", true)
+	_occluder.visible = false
 	_sprite.animation = "open"
 	_sprite.frame = 0
 
 func open() -> void:
 	_collision.set_deferred("disabled", true)
+	_occluder.visible = false
 	_sprite.play("open")
 
 func close() -> void:
 	_collision.set_deferred("disabled", false)
+	_occluder.visible = true
 	_sprite.play_backwards("open")  # rewinds the open animation as a close
 
 func set_open(open: bool) -> void:
