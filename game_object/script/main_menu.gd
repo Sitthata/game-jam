bede1807c31efe33264a0f51f26f1615db495618
Sitthata <aprils96950@gmail.com>
2026-04-29 22:08:35 +0700

    fix: fixing lever logic and remove exit button

--- game_object/script/main_menu.gd
+++ game_object/script/main_menu.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @onready var start_button: Button = $Control/StartButton
-@onready var exit_button: Button = $Control/ExitButton
 @onready var player_sprite: AnimatedSprite2D = $PlayerSprite
 
 func _ready() -> void:
@@ -10,7 +9,3 @@ func _ready() -> void:
 
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://game_object/scene/tile_map.tscn")
-
-
-func _on_exit_pressed() -> void:
-	get_tree().exit()
