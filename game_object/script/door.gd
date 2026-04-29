22cdf2ad6c976103b7bdaf4c8955439951486b27
Sitthata <aprils96950@gmail.com>
2026-04-29 23:21:59 +0700

    feat: add required 2 lever door

--- game_object/script/door.gd
+++ game_object/script/door.gd
@@ -4,13 +4,45 @@ extends StaticBody2D
 @onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var _occluder: LightOccluder2D = $LightOccluder2D
 
+@export var lever_a: NodePath
+@export var lever_b: NodePath
+
 var is_open: bool = true
+var _lever_states: Dictionary = {}
 
 func _ready() -> void:
 	_collision.set_deferred("disabled", true)
 	_occluder.visible = false
 	_sprite.animation = "open"
 	_sprite.frame = _sprite.sprite_frames.get_frame_count("open") - 1
+
+	_connect_lever(lever_a)
+	_connect_lever(lever_b)
+	if not _lever_states.is_empty():
+		_update_dual_lever_state.call_deferred()
+
+func _connect_lever(path: NodePath) -> void:
+	if path.is_empty():
+		return
+	var lever = get_node(path)
+	if lever == null:
+		return
+	_lever_states[lever] = lever.is_on
+	lever.toggled.connect(_on_lever_toggled.bind(lever))
+
+func _on_lever_toggled(is_on: bool, lever: Node) -> void:
+	_lever_states[lever] = is_on
+	_update_dual_lever_state()
+
+func _update_dual_lever_state() -> void:
+	if _lever_states.size() < 2:
+		return
+	var all_on := true
+	for v in _lever_states.values():
+		if not v:
+			all_on = false
+			break
+	set_open(all_on)
 
 func open() -> void:
 	if is_open:
